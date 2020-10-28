2874e48c36f14ce7c4d65383a608d242bfd9e079
fabricio <fabriciobtcc@gmail.com>
2020-10-28 02:36:00 -0300

    adicionado tela de cadastro de usuario

--- app/src/main/java/com/fabricio/login/MainActivity.java
+++ app/src/main/java/com/fabricio/login/MainActivity.java
@@ -2,13 +2,42 @@ package com.fabricio.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText textemail;
+    private EditText textsenha;
+    private Button btnEnter;
+    private TextView textaccount;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textaccount = findViewById(R.id.id_textCadastar);
+
+
+        //tela de cadastro
+        textaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //intenção entre as telas
+                Intent intent = new Intent(MainActivity.this, CadastrarUser.class);
+                //ação
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/release/java/com/fabricio/login/CadastarUser.java
@@ -0,0 +1,14 @@
+package com.fabricio.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastarUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastar_user);
+    }
+}
